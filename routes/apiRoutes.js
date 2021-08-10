c018603ec192654a222fc1c9b126a9bb2d3c406d
Izzy <browncans@gmail.com>
2021-08-10 00:10:03 -0700

    routes for api

---
+++ routes/apiRoutes.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", async (req, res) => {
+  try {
+    const workoutData = await Workout.find();
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+  try {
+    const workoutData = await Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    });
+    res.json(workoutData);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.post("/api/workouts", async (req, res) => {
+  try {
+    const workoutData = await Workout.create(req.body);
+    res.json(workoutData);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+    const workoutData = await Workout.find();
+    res.json(workoutData);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+module.exports = router;
